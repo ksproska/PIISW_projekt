683987ef022e1906b30ae45002aaabdf6cd628ac
LAPTOP-LE5RDGMH\Michal <micjac2000@o2.pl>
2023-05-04 11:48:44 +0200

    Added swagger-ui + endpoint for ticket verification

--- BackendApp/src/main/java/com/example/backendapp/repository/TicketRepository.java
+++ BackendApp/src/main/java/com/example/backendapp/repository/TicketRepository.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository<T extends Ticket> extends JpaRepository<T,Long> {}
+public interface TicketRepository extends JpaRepository<Ticket, Long> {}
--- BackendApp/src/main/java/com/example/backendapp/service/TicketService.java
+++ BackendApp/src/main/java/com/example/backendapp/service/TicketService.java
@@ -1,9 +1,28 @@
 package com.example.backendapp.service;
 
+import com.example.backendapp.model.ticket.Ticket;
+import com.example.backendapp.repository.TicketRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class TicketService {
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    public Optional<Ticket> getTicketById(long id) { return ticketRepository.findById(id); }
+
+    public boolean verifyTicket(long id, String tramId) throws NoSuchElementException
+    {
+        // TODO validation
+        var ticket = ticketRepository.findById(id);
+        if (ticket.isEmpty()) throw new NoSuchElementException("Ticket with id '" + id + "' not found");
+        return ticket.get().verifyTicket(tramId, Calendar.getInstance().getTime());
+    }
 }
